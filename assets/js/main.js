cd8222d1ffb5fb9def1506e37bf8241696b5d97e
MinboChung <minbo_chung@hotmail.com>
2022-11-19 01:02:57 +0100

    Updated Experiences section

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,6 +104,7 @@
     onscroll(document, toggleMobileTab)
   }
 
+
   on('click', '.mobile-nav-toggle', function(e) {
     select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
